6e45009ca576ba4421802765bd2de3dfef677e30
Ibrahim Ozkan <51259479+ibrahimozkn@users.noreply.github.com>
2025-01-12 00:48:22 +0100

    chore: refactor Flutter code to use const constructors and update dependencies

--- example/lib/screens/homePage.dart
+++ example/lib/screens/homePage.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import '../utils/strings.dart';
 
 class HomePage extends StatelessWidget {
-  HomePage({Key? key}) : super(key: key);
+  const HomePage({super.key});
 
   static const route = '/';
   static const routename = 'HomePage';
@@ -14,7 +14,7 @@ class HomePage extends StatelessWidget {
     print('${HomePage.routename} built');
     return Scaffold(
       appBar: AppBar(
-        title: Text(HomePage.routename),
+        title: const Text(HomePage.routename),
       ),
       body: Center(
         child: Column(
@@ -23,15 +23,18 @@ class HomePage extends StatelessWidget {
             ElevatedButton(
               onPressed: () async {
                 // Optional: customize the authorization scope
-                //           If this is not specified, by default it will be generated a token pair with rights to access to all data 
+                //           If this is not specified, by default it will be generated a token pair with rights to access to all data
                 //           If you are wrong in specifying the auth scopes, errors will be possible.
                 //           As an example, here we are requesting access rights to profile and SpO2 data only.
                 //           The following can be requested: FitbitAuthScope.ACTIVITY, FitbitAuthScope.CARDIO_FITNESS, FitbitAuthScope.HEART_RATE, FitbitAuthScope.LOCATION, FitbitAuthScope.NUTRITION, FitbitAuthScope.OXYGEN_SATURATION, FitbitAuthScope.PROFILE, FitbitAuthScope.RESPIRATORY_RATE, FitbitAuthScope.SETTINGS, FitbitAuthScope.SLEEP, FitbitAuthScope.SOCIAL, FitbitAuthScope.TEMPERATURE
-                final List<FitbitAuthScope> scopeList = [FitbitAuthScope.PROFILE, FitbitAuthScope.OXYGEN_SATURATION];
+                final List<FitbitAuthScope> scopeList = [
+                  FitbitAuthScope.PROFILE,
+                  FitbitAuthScope.OXYGEN_SATURATION
+                ];
                 // Optional: customize the token lifetime in seconds. It can be 3600 (1 hour), 28800 (8 hours), 86400 (1 day), 604800 (1 week), 2592000 (30 days), or 31536000 (1 year)
                 //           If this is not specified, by default it will be 28800.
                 //           As an example, here we are requesting a lifetime of 3600 seconds
-                final int expiresIn = 3600;
+                const int expiresIn = 3600;
 
                 // Authorize the app and get the Fitbit credentials
                 FitbitCredentials? fitbitCredentials =
@@ -54,7 +57,7 @@ class HomePage extends StatelessWidget {
                 //Fetch yesterday's data
                 final spO2Data =
                     await fitbitSpO2DataManager.fetch(FitbitSpO2APIURL.day(
-                  date: DateTime.now().subtract(Duration(days: 1)),
+                  date: DateTime.now().subtract(const Duration(days: 1)),
                   fitbitCredentials: fitbitCredentials!,
                 )) as List<FitbitSpO2Data>;
                 print(spO2Data);
@@ -63,10 +66,12 @@ class HomePage extends StatelessWidget {
                   final snackBar = SnackBar(
                       content: Text(
                           'Yesterday your SpO2 was ${spO2Data[0].avgValue}% on average!'));
+
+                  if (!context.mounted) return;
                   ScaffoldMessenger.of(context).showSnackBar(snackBar);
                 }
               },
-              child: Text('Tap to authorize and fetch data'),
+              child: const Text('Tap to authorize and fetch data'),
             ),
           ],
         ),
