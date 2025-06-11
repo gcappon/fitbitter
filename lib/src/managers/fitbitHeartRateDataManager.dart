da557fbcda0c17788950e36fe0c12865dbe1a3e6
klnfreedom <klnfreedom@gmail.com>
2025-06-11 16:58:54 +0300

    feat: add new plugins for desktop webview, URL launching, and window management

--- lib/src/managers/fitbitHeartRateDataManager.dart
+++ lib/src/managers/fitbitHeartRateDataManager.dart
@@ -10,8 +10,7 @@ import 'package:fitbitter/src/managers/fitbitDataManager.dart';
 /// [FitbitHeartDataManager] is a class the manages the requests related to
 /// [FitbitHeartRateData].
 class FitbitHeartDataManager extends FitbitDataManager {
-  FitbitHeartDataManager(
-      {required String clientID, required String clientSecret})
+  FitbitHeartDataManager({required String clientID, required String clientSecret})
       : super(
           clientID: clientID,
           clientSecret: clientSecret,
@@ -27,40 +26,29 @@ class FitbitHeartDataManager extends FitbitDataManager {
     logger.i('$response');
 
     //Extract data and return them
-    List<FitbitData> ret =
-        _extractFitbitHeartData(response, fitbitUrl.fitbitCredentials!.userID);
+    List<FitbitData> ret = _extractFitbitHeartData(response, fitbitUrl.fitbitCredentials!.userID);
     return ret;
   } // fetch
 
   /// A private method that extracts [FitbitHeartRateData] from the given response.
-  List<FitbitHeartRateData> _extractFitbitHeartData(
-      dynamic response, String? userId) {
+  List<FitbitHeartRateData> _extractFitbitHeartData(dynamic response, String? userId) {
     final data = response['activities-heart'];
-    List<FitbitHeartRateData> heartDataPoints =
-        List<FitbitHeartRateData>.empty(growable: true);
+    List<FitbitHeartRateData> heartDataPoints = List<FitbitHeartRateData>.empty(growable: true);
 
     for (var record = 0; record < data.length; record++) {
       heartDataPoints.add(FitbitHeartRateData(
         userID: userId,
         dateOfMonitoring: DateTime.parse(data[record]['dateTime']),
-        caloriesOutOfRange: data[record]['value']['heartRateZones'][0]
-                ['caloriesOut']
-            .toDouble(),
+        caloriesOutOfRange: data[record]['value']['heartRateZones'][0]['caloriesOut']?.toDouble(),
         minimumOutOfRange: data[record]['value']['heartRateZones'][0]['min'],
-        minutesOutOfRange: data[record]['value']['heartRateZones'][0]
-            ['minutes'],
-        caloriesFatBurn: data[record]['value']['heartRateZones'][1]
-                ['caloriesOut']
-            .toDouble(),
+        minutesOutOfRange: data[record]['value']['heartRateZones'][0]['minutes'],
+        caloriesFatBurn: data[record]['value']['heartRateZones'][1]['caloriesOut']?.toDouble(),
         minimumFatBurn: data[record]['value']['heartRateZones'][1]['min'],
         minutesFatBurn: data[record]['value']['heartRateZones'][1]['minutes'],
-        caloriesCardio: data[record]['value']['heartRateZones'][2]
-                ['caloriesOut']
-            .toDouble(),
+        caloriesCardio: data[record]['value']['heartRateZones'][2]['caloriesOut']?.toDouble(),
         minimumCardio: data[record]['value']['heartRateZones'][2]['min'],
         minutesCardio: data[record]['value']['heartRateZones'][2]['minutes'],
-        caloriesPeak: data[record]['value']['heartRateZones'][3]['caloriesOut']
-            .toDouble(),
+        caloriesPeak: data[record]['value']['heartRateZones'][3]['caloriesOut']?.toDouble(),
         minimumPeak: data[record]['value']['heartRateZones'][3]['min'],
         minutesPeak: data[record]['value']['heartRateZones'][3]['minutes'],
         restingHeartRate: data[record]['value']['restingHeartRate'],
