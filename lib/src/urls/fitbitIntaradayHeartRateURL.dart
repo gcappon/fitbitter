3cc3e2d815f78d35ae5fdf7588d15b91f9e4f79e
klnfreedom <klnfreedom@gmail.com>
2025-06-18 09:05:26 +0300

    feat: remove unused factory methods from FitbitIntradayHeartRateAPIURL for cleaner code

--- lib/src/urls/fitbitIntaradayHeartRateURL.dart
+++ lib/src/urls/fitbitIntaradayHeartRateURL.dart
@@ -7,32 +7,4 @@ class FitbitIntradayHeartRateAPIURL extends FitbitAPIURL {
     required FitbitCredentials? fitbitCredentials,
     required String url,
   }) : super(url: url, fitbitCredentials: fitbitCredentials);
-
-  factory FitbitIntradayHeartRateAPIURL.fromUrl({
-    required FitbitCredentials fitbitCredentials,
-    required String url,
-  }) {
-    final authedUrl = url.replaceFirst('-', fitbitCredentials.userID);
-    return FitbitIntradayHeartRateAPIURL(
-      fitbitCredentials: fitbitCredentials,
-      url: authedUrl,
-    );
-  }
-
-  factory FitbitIntradayHeartRateAPIURL.range({
-    required FitbitCredentials fitbitCredentials,
-    required DateTime startTime,
-    required DateTime endTime,
-  }) {
-    final dateStr = Formats.onlyDayDateFormatTicks.format(startTime);
-    final startTimeStr = Formats.onlyTimeWithSeconds.format(startTime);
-    final endTimeStr = Formats.onlyTimeWithSeconds.format(endTime);
-
-    final url =
-        'https://api.fitbit.com/1/user/${fitbitCredentials.userID}/activities/heart/date/$dateStr/$dateStr/1sec/time/$startTimeStr/$endTimeStr.json';
-    return FitbitIntradayHeartRateAPIURL(
-      fitbitCredentials: fitbitCredentials,
-      url: url,
-    );
-  }
 }
