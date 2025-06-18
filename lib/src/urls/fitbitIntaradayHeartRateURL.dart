7f5c2d2e467069e1252e6601ba3032b9fd604716
klnfreedom <klnfreedom@gmail.com>
2025-06-18 09:05:37 +0300

    feat: remove unused factory methods from FitbitIntradayHeartRateAPIURL for cleaner code

--- lib/src/urls/fitbitIntaradayHeartRateURL.dart
+++ lib/src/urls/fitbitIntaradayHeartRateURL.dart
@@ -1,6 +1,5 @@
 import 'package:fitbitter/src/fitbitConnector.dart';
 import 'package:fitbitter/src/urls/fitbitAPIURL.dart';
-import 'package:fitbitter/src/utils/formats.dart';
 
 class FitbitIntradayHeartRateAPIURL extends FitbitAPIURL {
   FitbitIntradayHeartRateAPIURL({
